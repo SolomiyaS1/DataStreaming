0c68571688de72edbb7f8a4a85e3c1078b4fa5d4
KhrystynaHranishak <kristinka.granishak@gmail.com>
2021-07-06 13:24:36 +0300

    Corrected the final run script.

--- run_app.sh
+++ run_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd Services
-python generator.py &
-python language_detector.py &
 python sentimment_detector.py &
+python language_detector.py &
 python entity_detector.py &
 python statistic_generator_1.py &
 python statistic_generator_2.py &
+python generator.py
